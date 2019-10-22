f80b3c66f4e12e3dd8e33040c3c1778af6270933
Tls_1 <vbjhomer@gmail.com>
2019-10-21 19:56:26 -0500

    arduino

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 $( document ).ready(function() {
 
 	function connect(board, type){
-		let url = 'http://kerberosdevelopment.com/registrar_evento.php?placa=' + board + '&tipo=' + type;
+		let url = 'http://www.kerberosdevelopment.com/registrar_evento.php?placa=' + board + '&tipo=' + type;
 		$.get( url, function( data ) {
 	  		console.log(data);
 		});
 	}
+
+	//connect('ejemplo','S');
 });
